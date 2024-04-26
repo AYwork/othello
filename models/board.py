d62758f06226a2366feff8d40792234a9dba8c0e
yoshiki abe <abeyoshiki@abesachikinoMacBook-Pro.local>
2024-04-26 11:22:59 +0900

    4/26 11:22

--- models/board.py
+++ models/board.py
@@ -33,37 +33,227 @@ class Board:
             return True
         return False
     
+
     def judge_to_put(self, px: int, py: int, player: Union[Player, Cpu]) -> bool:
-        a = []
-        aroundthepiece = [
-        (-1,-1),
-        (0,-1),
-        (1,-1),
-        (-1,0),
-        (1,0),
-        (-1,1),
-        (0,1),
-        (1,1)]
-        for row in range(0, 8):
-            for column in range(0, 8):
-                if self.pieces[row][column].state == Color.SPACE:
-                    for ay, ax in aroundthepiece:
-                        x = column + ax
-                        y = row + ay
-                        if 0 <= x < 8 and 0 <= y < 8 and self.pieces[y][x].state != player.color: #マスの範囲内で、プレイヤーの色と異なる色がある場合、その方向を引き続きチェック
-                            while True:
-                                x += ax
-                                y += ay
-                                if 0 <= x < 8 and 0 <= y < 8 and self.pieces[y][x].state != player.color:#プレイヤーの色と異なる色がある場合、その方向を引き続きチェック
-                                    continue
-                                elif 0 <= x < 8 and 0 <= y < 8 and self.pieces[y][x].state == player.color:#プレイヤーの色と同じ色がある場合、置ける場所として保存
-                                    a.append((column, row))
-                                    break
-                                else:
-                                    break
-        # print(a)
-        if not self.is_already_put(px, py) and (px, py) in a:
-            return False
+        flag = True
+        x_offset = px
+        y_offset = py
+        ########################################
+        # 行
+        ########################################
+        # コマを置いた左側
+        target_x_left = x_offset
+        for x_left in range(x_offset - 1, -1, -1):
+            if self.pieces[y_offset][x_left].state != player.color and self.is_already_put(x_left, y_offset): #違う色なら
+                continue
+            # print(self.pieces[y_offset][x_left].state)
+            # print(self.last_puted_color)
+            # print(type(self.pieces[y_offset][x_left].state))
+            # print(type(self.last_puted_color))
+            if (self.pieces[y_offset][x_left].state == player.color) and (x_left is not x_offset):
+                target_x_left = x_left
+                flag = False
+                break
+            else:
+                break
+
+        # コマを置いた右側
+        target_x_right = x_offset
+        for x_right in range(x_offset + 1, 8):
+            if self.pieces[y_offset][x_right].state != player.color and self.is_already_put(x_right, y_offset):
+                continue
+            if (self.pieces[y_offset][x_right].state == player.color) and (x_right is not x_offset):
+                target_x_right = x_right
+                flag = False
+                break
+            else:
+                break
+
+        # 書き換え
+        # for x in range(x_offset - 1, target_x_left, -1):
+        #     self.pieces[y_offset][x].reverse_piece()
+            
+
+        # for x in range(x_offset + 1, target_x_right):
+        #     self.pieces[y_offset][x].reverse_piece()
+
+        ########################################
+        # 列
+        ########################################
+        # コマを置いた上側
+        target_y_upper = y_offset
+        for y_upper in range(y_offset - 1, -1, -1):
+            if self.pieces[y_upper][x_offset].state != player.color and self.is_already_put(x_offset, y_upper):
+                continue
+            if (self.pieces[y_upper][x_offset].state == player.color) and (y_upper is not y_offset):
+                target_y_upper = y_upper
+                flag = False
+                break
+            else:
+                break
+
+        # コマを置いた下側
+        target_y_lower = y_offset
+        for y_lower in range(y_offset + 1, 8):
+            if self.pieces[y_lower][x_offset].state != player.color and self.is_already_put(x_offset, y_lower):
+                continue
+            if (self.pieces[y_lower][x_offset].state == player.color) and (y_lower is not y_offset):
+                target_y_lower = y_lower
+                flag = False
+                break
+            else:
+                break
+
+        # 書き換え
+        # for y in range(y_offset - 1, target_y_upper, -1):
+        #     self.pieces[y][x_offset].reverse_piece()
+         
+        # for y in range(y_offset + 1, target_y_lower):
+        #     self.pieces[y][x_offset].reverse_piece()
+         
+
+
+        ########################################
+        # 右上がり斜め
+        ########################################
+        # コマを置いた左下側
+        target_x_left = x_offset
+        target_y_lower = y_offset
+        for i in range(1, 8):
+            x_left = x_offset - i
+            y_lower = y_offset + i
+            if x_left < 0 or y_lower > 7: #壁にぶつかったら
+                break
+            if self.pieces[y_lower][x_left].state != player.color and self.is_already_put(x_left,y_lower):  # 違う色なら
+                continue
+            if (self.pieces[y_lower][x_left].state == player.color) and (x_left is not x_offset) and (y_lower is not y_offset):
+                target_x_left = x_left
+                target_y_lower = y_lower
+                flag = False
+                break
+            else:
+                break
+
+        # コマを置いた右上側
+        target_x_right = x_offset
+        target_y_upper = y_offset
+        for i in range(1, 8):
+            x_right = x_offset + i
+            y_upper = y_offset - i
+            if x_right > 7 or y_upper < 0:  # 壁にぶつかったら
+                break
+            if self.pieces[y_upper][x_right].state != player.color and self.is_already_put(x_right,y_upper):
+                continue
+            if (self.pieces[y_upper][x_right].state == player.color) and (x_right is not x_offset) and (y_upper is not y_offset):
+                target_x_right = x_right
+                target_y_upper = y_upper
+                flag = False
+                break
+            else:
+                break
+
+        # # 書き換え
+        # for i in range(1, abs(target_x_left - x_offset)):
+        #     x = x_offset - i
+        #     y = y_offset + i
+        #     self.pieces[y][x].reverse_piece()
+    
+        # for i in range(1, abs(target_x_right - x_offset)):
+        #     x = x_offset + i
+        #     y = y_offset - i
+        #     self.pieces[y][x].reverse_piece()
+            
+        ########################################
+        # 右下がり斜め
+        ########################################
+        # コマを置いた左上側
+        target_x_left = x_offset
+        target_y_upper = y_offset
+        for i in range(1, 8):
+            x_left = x_offset - i
+            y_upper = y_offset - i
+            if x_left < 0 or y_upper < 0: #壁にぶつかったら
+                break
+            if self.pieces[y_upper][x_left].state != player.color and self.is_already_put(x_left,y_upper):  # 違う色なら
+                continue
+            if (self.pieces[y_upper][x_left].state == player.color) and (x_left is not x_offset) and (y_upper is not y_offset):
+                target_x_left = x_left
+                target_y_upper = y_upper
+                flag = False
+                break
+            else:
+                break
+
+        # コマを置いた右上側
+        target_x_right = x_offset
+        target_y_lower = y_offset
+        for i in range(1, 8):
+            x_right = x_offset + i
+            y_lower = y_offset + i
+            if x_right > 7 or y_lower > 7:  # 壁にぶつかったら
+                break
+            # print(type(self.pieces[y_lower][x_right]))
+            if self.pieces[y_lower][x_right].state != player.color and self.is_already_put(x_right,y_lower):
+                continue
+            if (self.pieces[y_lower][x_right].state == player.color) and (x_right is not x_offset) and (y_lower is not y_offset):
+                target_x_right = x_right
+                target_y_lower = y_lower
+                flag = False
+                break
+            else:
+                break
+
+        # # 書き換え
+        # for i in range(1, abs(target_x_left - x_offset)):
+        #     x = x_offset - i
+        #     y = y_offset - i
+        #     self.pieces[y][x].reverse_piece()
+          
+
+        # for i in range(1, abs(target_x_right - x_offset)):
+        #     x = x_offset + i
+        #     y = y_offset + i
+        #     self.pieces[y][x].reverse_piece()
+    
+        return flag
+
+
+
+
+
+
+    # def judge_to_put(self, px: int, py: int, player: Union[Player, Cpu]) -> bool:
+    #     a = []
+    #     aroundthepiece = [
+    #     (-1,-1),
+    #     (0,-1),
+    #     (1,-1),
+    #     (-1,0),
+    #     (1,0),
+    #     (-1,1),
+    #     (0,1),
+    #     (1,1)]
+    #     for row in range(0, 8):
+    #         for column in range(0, 8):
+    #             if self.pieces[row][column].state == Color.SPACE:
+    #                 for ay, ax in aroundthepiece:
+    #                     x = column + ax
+    #                     y = row + ay
+    #                     if 0 <= x < 8 and 0 <= y < 8 and self.pieces[y][x].state != player.color: #マスの範囲内で、プレイヤーの色と異なる色がある場合、その方向を引き続きチェック
+    #                         while True:
+    #                             x += ax
+    #                             y += ay
+    #                             if 0 <= x < 8 and 0 <= y < 8 and self.pieces[y][x].state != player.color:#プレイヤーの色と異なる色がある場合、その方向を引き続きチェック
+    #                                 continue
+    #                             elif 0 <= x < 8 and 0 <= y < 8 and self.pieces[y][x].state == player.color:#プレイヤーの色と同じ色がある場合、置ける場所として保存
+    #                                 a.append((column, row))
+    #                                 break
+    #                             else:
+    #                                 break
+    #     # print(a)
+    #     if not self.is_already_put(px, py) and (px, py) in a:
+    #         return False
+        
 
 
 
